8311803a325b10374ac3b63f7c4379b7ddef9094
unknown <pritampaul9798@gmail.com>
2021-04-27 14:20:34 +0530

    [Pritam] Added : code to pass second test case

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,8 +1,14 @@
 public class InvoiceGenerator {
     private double MinimumCostPerKM = 10;
     private int CostPerMinute = 1;
+    private double MinimumFare = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance * MinimumCostPerKM + time * CostPerMinute;
+        double totalFare = distance * MinimumCostPerKM + time * CostPerMinute;
+        if(totalFare<MinimumFare)
+        {
+            return MinimumFare;
+        }
+        return totalFare;
     }
 }
